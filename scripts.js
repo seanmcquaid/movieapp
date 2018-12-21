c6698c4dc17d7e7a08d3444a53b9a15393d1b547
seanmcquaid <sean.mcquaid2@verizon.net>
2018-12-20 22:15:37 -0500

    fixed UI so it looked better and made javascript onyl display the top 6 movies!

--- scripts.js
+++ scripts.js
@@ -7,13 +7,35 @@ const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
 
 $.getJSON(nowPlayingUrl, (movieData)=>{
     console.log(movieData);
-    movieData.results.forEach((movie)=>{
-        const posterUrl = `${imageBaseUrl}w300${movie.poster_path}`;
-        const newHTML = `
-        <div class="col-4">
+    
+    for(let i =0; i < 6; i++){
+        let movieInfo = movieData.results[i];
+        const posterUrl = `${imageBaseUrl}w300${movieInfo.poster_path}`;
+        let newHTML = `
+        <div class="posters movie-poster-${i} col-4">
             <img src="${posterUrl}"/>
-            ${movie.title}
-        </div>`;
-        $("#movie-grid").append(newHTML);
-    });
+            <div class="movie-text">
+                <div class="title">${movieInfo.title}</div>
+                <div class="title">Release Date: ${movieInfo.release_date}</div>
+                <div class="title">Popularity: ${movieInfo.popularity}</div>
+                <div class="title">Vote Count: ${movieInfo.vote_count}</div>
+                <div class="title">Vote Average: ${movieInfo.vote_average}</div>
+            </div>
+        </div>
+        `
+        if(i<3){
+            $(".top-three").append(newHTML);
+        } else{
+            $(".bottom-three").append(newHTML);
+        }
+    }
 });
+
+{/* 
+<div class="movie-text">
+<div class="title">${movie.title}</div>
+<div class="title">Release Date: ${movie.release_date}</div>
+<div class="title">Popularity: ${movie.popularity}</div>
+<div class="title">Vote Count: ${movie.vote_count}</div>
+<div class="title">Vote Average: ${movie.vote_average}</div>
+</div> */}
